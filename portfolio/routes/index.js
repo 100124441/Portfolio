cd58badbc4cfd81a9841e1c09574956f2a8f6988
100124441 <100124441@student.georgianc.on.ca>
2015-10-14 17:38:04 -0400

    update index.js

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Owen' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'Portfolio' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.post('/contact/contacted', function(req, res, next) {
+  fs.appendFile() {
+    
+  });
+  res.render('contacted', { title: 'Contact',
+                            message: 'Comment saved'
+  })
 });
 
 module.exports = router;
